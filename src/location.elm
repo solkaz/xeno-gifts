c6ccbbe0ddf042f8004e1427514b0432b53edaa5
JMH <jheld135@gmail.com>
2017-04-28 18:39:46 -0500

    Add every Location in the game

--- src/location.elm
+++ src/location.elm
@@ -8,13 +8,20 @@ type Location
   | EtherMine
   | SatoriMarsh
   | BionisInterior
+  | MaknaForest
   | FrontierVillage
   | ErythSea
   | Alcamoth
+  | HighEntiaTomb
+  | ValakMountain
+  | SwordValley
+  | GalahadFortress
   | FallenArm
   | MechonisField
   | CentralFactory
   | Agniratha
+  | PrisonIsland
+  | Other
 
 locationToString: Location -> String
 locationToString location =
@@ -25,15 +32,21 @@ locationToString location =
     BionisLeg -> "Bionis' Leg"
     EtherMine -> "Ether Mine"
     SatoriMarsh -> "Satori Marsh"
+    MaknaForest -> "Makna Forest"
     BionisInterior -> "Bionis' Interior"
     FrontierVillage -> "Frontier Village"
     ErythSea -> "Eryth Sea"
     Alcamoth -> "Alcamoth"
+    HighEntiaTomb -> "High  Entia Tomb"
+    ValakMountain -> "Valak Mountain"
+    SwordValley -> "Sword Valley"
+    GalahadFortress -> "Galahad Fortress"
     FallenArm -> "Fallen Arm"
     MechonisField -> "Mechonis Field"
     CentralFactory -> "Central Factory"
     Agniratha -> "Agniratha"
-
+    PrisonIsland -> "Prison Island"
+    Other -> "Other"
 locationSortingRank: Location -> Int
 locationSortingRank location =
   case location of
@@ -44,10 +57,17 @@ locationSortingRank location =
     EtherMine -> 5
     SatoriMarsh -> 6
     BionisInterior -> 7
+    MaknaForest -> 8
     FrontierVillage -> 9
     ErythSea -> 10
     Alcamoth -> 11
+    HighEntiaTomb -> 12
+    ValakMountain -> 13
+    SwordValley -> 14
+    GalahadFortress -> 15
     FallenArm -> 16
     MechonisField -> 17
     CentralFactory -> 18
     Agniratha -> 19
+    PrisonIsland -> 20
+    Other -> 21
