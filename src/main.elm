fcc5f439d5ae45344a69cc10693e7aa4126a81ee
JMH <jheld135@gmail.com>
2017-07-31 20:18:10 -0500

    Refactor view to View.elm

--- src/main.elm
+++ src/main.elm
@@ -1,66 +1,15 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, h1, input, text)
-import Html.Attributes exposing (placeholder)
-import Html.Events exposing (onInput)
-import Table
-import Item exposing (Item, items)
-import FriendshipColumn exposing (friendshipColumn)
-import ItemNameColumn exposing (itemNameColumn)
-import ItemTypeColumn exposing (itemTypeColumn)
-import LocationColumn exposing (locationColumn)
-import Character exposing (..)
+import Html exposing (program)
 import Model exposing (Model)
 import Msg exposing (Msg)
+import View exposing (view)
 
 
 main =
-    Html.program
+    program
         { init = Model.init
         , update = Model.update
         , view = view
-        , subscriptions = \_ -> Sub.none
-        }
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view { items, tableState, query } =
-    let
-        lowerQuery =
-            String.toLower query
-
-        matchedItems =
-            if String.isEmpty lowerQuery then
-                items
-            else
-                List.filter (String.contains lowerQuery << String.toLower << .name) items
-    in
-        div []
-            [ h1 [] [ text "Xeno-Gifts" ]
-            , input [ placeholder "Item name", onInput Msg.SetQuery ] []
-            , Table.view config tableState matchedItems
-            ]
-
-
-config : Table.Config Item Msg
-config =
-    Table.config
-        { toId = .name
-        , toMsg = Msg.SetTableState
-        , columns =
-            [ itemNameColumn
-            , locationColumn
-            , itemTypeColumn
-            , friendshipColumn Shulk
-            , friendshipColumn Fiora
-            , friendshipColumn Dunban
-            , friendshipColumn Reyn
-            , friendshipColumn Sharla
-            , friendshipColumn Riki
-            , friendshipColumn Melia
-            ]
+        , subscriptions = Model.subscriptions
         }
---
+++ src/view.elm
@@ -0,0 +1,53 @@
+module View exposing (..)
+
+import Html exposing (Html, div, h1, input, text)
+import Html.Attributes exposing (placeholder)
+import Html.Events exposing (onInput)
+import Table
+import Item exposing (Item, items)
+import FriendshipColumn exposing (friendshipColumn)
+import ItemNameColumn exposing (itemNameColumn)
+import ItemTypeColumn exposing (itemTypeColumn)
+import LocationColumn exposing (locationColumn)
+import Character exposing (..)
+import Model exposing (Model)
+import Msg exposing (Msg)
+
+
+view : Model -> Html Msg
+view { items, tableState, query } =
+    let
+        lowerQuery =
+            String.toLower query
+
+        matchedItems =
+            if String.isEmpty lowerQuery then
+                items
+            else
+                List.filter (String.contains lowerQuery << String.toLower << .name) items
+    in
+        div []
+            [ h1 [] [ text "Xeno-Gifts" ]
+            , input [ placeholder "Item name", onInput Msg.SetQuery ] []
+            , Table.view config tableState matchedItems
+            ]
+
+
+config : Table.Config Item Msg
+config =
+    Table.config
+        { toId = .name
+        , toMsg = Msg.SetTableState
+        , columns =
+            [ itemNameColumn
+            , locationColumn
+            , itemTypeColumn
+            , friendshipColumn Shulk
+            , friendshipColumn Fiora
+            , friendshipColumn Dunban
+            , friendshipColumn Reyn
+            , friendshipColumn Sharla
+            , friendshipColumn Riki
+            , friendshipColumn Melia
+            ]
+        }
